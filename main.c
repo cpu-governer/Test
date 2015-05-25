c978f96b751703c0e81a54137d03414908de9fe2
cpu-governer <chris.sanchez626@gmail.com>
2015-05-25 16:49:20 -0700

    lel

--- main.c
+++ main.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
+#include <math.h>
 
 int main(int argc, char** argv)
 {
-        printf("Hello world");
+        printf("Hello world\n");
+	printf("2 ^ 5 = %lu\n", pow(2, 5));
         return 0;
 }
